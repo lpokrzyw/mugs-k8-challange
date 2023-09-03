13d54b7852454537ce103a05392ab0106de3e7f1
Lukasz Pokrzywka <lpokrzyw@cisco.com>
2023-09-03 15:59:20 +0200

    adding java service and working node endpoint

--- node-service/app.js
+++ node-service/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+
 const app = express()
-const port = 3000
+const port = 8889
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send({ status: 'SUCCESS' });
+  console.log(req.body);
+  const appName = JSON.stringify(req.body);
 })
 
 app.listen(port, () => {
